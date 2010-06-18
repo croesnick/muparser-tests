5cfce9136abde5e84b7d2e3e791e23c3c8196880
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-06-18 16:07:19 +0200

    Extend builtin operators test

--- testsuite-1/builtin-operators.cxx
+++ testsuite-1/builtin-operators.cxx
@@ -4,11 +4,17 @@
 
 #include <muParser/muParser.h>
 
+#include <limits>
+
 class BuiltinOperatorsTest : public CppUnit::TestFixture {
 public:
     CPPUNIT_TEST_SUITE (BuiltinOperatorsTest);
     CPPUNIT_TEST (testUnaryMinus);
     CPPUNIT_TEST (testAdd);
+    CPPUNIT_TEST (testSubtract);
+    CPPUNIT_TEST (testMultiply);
+    CPPUNIT_TEST (testDivide);
+    CPPUNIT_TEST (testPower);
     CPPUNIT_TEST_SUITE_END ();
 
     void testUnaryMinus () {
@@ -63,6 +69,126 @@ public:
         parser.SetExpr ("-2 + 3 + 4");
         res = parser.Eval ();
         CPPUNIT_ASSERT_DOUBLES_EQUAL (5.0, res, 0.0);
+    }
+
+    void testSubtract () {
+        mu::Parser parser;
+        double res;
+
+        parser.SetExpr ("2 - 2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (0.0, res, 0.0);
+
+        parser.SetExpr ("2.5 - 2.5");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (0.0, res, 0.0);
+
+        parser.SetExpr ("2.5 - (-2.5)");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (5.0, res, 0.0);
+
+        parser.SetExpr ("2.5 - -2.5");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (5.0, res, 0.0);
+
+        parser.SetExpr ("2 - 3 + 4");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (3.0, res, 0.0);
+
+        parser.SetExpr ("-2 + 3 - 4");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (-3.0, res, 0.0);
+    }
+
+    void testMultiply () {
+        mu::Parser parser;
+        double res;
+
+        parser.SetExpr ("2 * 2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (4.0, res, 0.0);
+
+        parser.SetExpr ("2.5 * 2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (5.0, res, 0.0);
+
+        parser.SetExpr ("2.5 * (-2)");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (-5.0, res, 0.0);
+
+        parser.SetExpr ("-2.5 * -2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (5.0, res, 0.0);
+
+        parser.SetExpr ("2 * 3 * 4");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (24.0, res, 0.0);
+
+        parser.SetExpr ("-2 * 3 * 4");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (-24.0, res, 0.0);
+    }
+
+    void testDivide () {
+        mu::Parser parser;
+        double res;
+
+        parser.SetExpr ("2 / 2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (1.0, res, 0.0);
+
+        parser.SetExpr ("2.5 / 2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (1.25, res, 0.0);
+
+        parser.SetExpr ("2.5 / (-2)");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (-1.25, res, 0.0);
+
+        parser.SetExpr ("2 / 3 / 4");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (0.1666666666, res, 0.0001);
+
+        parser.SetExpr ("2 / 0");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (std::numeric_limits<double>::infinity (), res, 0.0);
+    }
+
+    void testPower () {
+        mu::Parser parser;
+        double res;
+
+        parser.SetExpr ("2^2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (4.0, res, 0.0);
+
+        parser.SetExpr ("4^0.5");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (2, res, 0.0);
+        
+        parser.SetExpr ("4^0.5^3");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (8, res, 0.0);
+
+        parser.SetExpr ("4^(-2)");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (0.0625, res, 0.0);
+
+        parser.SetExpr ("4^(2)");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (16, res, 0.0);
+
+        parser.SetExpr ("(-3)^2");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (9.0, res, 0.0);
+
+        parser.SetExpr ("(-3)^3");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (-27.0, res, 0.0);
+
+        parser.SetExpr ("27^(1/3)");
+        res = parser.Eval ();
+        CPPUNIT_ASSERT_DOUBLES_EQUAL (3, res, 0.0);
     }
 };
 
