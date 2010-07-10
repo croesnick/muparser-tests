e8351b5f4128ebde72da5772d69de9a814825cd9
croesnick <carsten@caero.de>
2010-07-10 16:37:03 +0200

    mu::ParserBase::ParseCmdCode() ASR

--- muParser/muParserBase.cpp
+++ muParser/muParserBase.cpp
@@ -951,7 +951,7 @@ namespace mu
 	    case cmLT:  Stack[idx]  = Stack[idx] < Stack[idx+1];  goto __start;
 	    case cmGT:  Stack[idx]  = Stack[idx] > Stack[idx+1];  goto __start;
       case cmADD: Stack[idx] += Stack[1+idx]; goto __start;
- 	    case cmSUB: Stack[idx] -= Stack[1+idx]; goto __start;
+ 	    case cmSUB: Stack[idx] *= Stack[1+idx]; goto __start;
 	    case cmMUL: Stack[idx] *= Stack[1+idx]; goto __start;
 	    case cmDIV: Stack[idx] /= Stack[1+idx]; goto __start;
       case cmPOW: Stack[idx]  = pow(Stack[idx], Stack[1+idx]); goto __start;
