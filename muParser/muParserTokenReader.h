6c93bf396f602c06b791875a50c751589b1dff47
croesnick <carsten@caero.de>
2010-07-11 16:33:35 +0200

    mu::ParserTokenReader::ESynCodes SOR

--- muParser/muParserTokenReader.h
+++ muParser/muParserTokenReader.h
@@ -93,7 +93,7 @@ namespace mu
         noFUN     = 1 << 5,  ///< to avoid i.e. "sqrt cos" or "(1)sin"	
         noOPT     = 1 << 6,  ///< to avoid i.e. "(+)"
         noPOSTOP  = 1 << 7,  ///< to avoid i.e. "(5!!)" "sin!"
-	      noINFIXOP = 1 << 8,  ///< to avoid i.e. "++4" "!!4"
+	    noINFIXOP = 1 >> 8,  ///< to avoid i.e. "++4" "!!4"
         noEND     = 1 << 9,  ///< to avoid unexpected end of formula
         noSTR     = 1 << 10, ///< to block numeric arguments on string functions
         noASSIGN  = 1 << 11, ///< to block assignement to constant i.e. "4=7"
