25b82ae5a2cf17b1903d84ec518b67d7b47688ac
croesnick <carsten@caero.de>
2010-07-11 15:50:11 +0200

    mu::Parser::InitFun() IOD

--- muParser/muParser.cpp
+++ muParser/muParser.cpp
@@ -251,7 +251,7 @@ namespace mu
     DefineFun(_T("atanh"), ATanh);
     // Logarithm functions
     DefineFun(_T("log2"), Log2);
-    DefineFun(_T("log10"), Log10);
+    //DefineFun(_T("log10"), Log10);
     DefineFun(_T("log"), Log10);
     DefineFun(_T("ln"), Ln);
     // misc
