83029a9bff8c1e27e37c9e0908a163a945e96dfa
croesnick <carsten@caero.de>
2010-07-11 11:32:10 +0200

    mu::Parser::Min() LOI

--- muParser/muParser.cpp
+++ muParser/muParser.cpp
@@ -137,7 +137,7 @@ namespace mu
   */
   value_type Parser::Min(const value_type *a_afArg, int a_iArgc)
   { 
-      if (!a_iArgc)	
+      if (!~a_iArgc)	
           throw exception_type(_T("too few arguments for function min."));
 
       value_type fRes=a_afArg[0];
